da69304d5f498851ad7dfb34c571678af1375e34
Luis Nunez <lnunez@kambda.com>
2023-03-21 18:23:10 -0500

    clean dependencies

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -7,15 +7,12 @@
 #include "generated_plugin_registrant.h"
 
 #include <flutter_webrtc/flutter_web_r_t_c_plugin.h>
-#include <flutter_window_close/flutter_window_close_plugin.h>
 #include <openvidu_client/openvidu_client_plugin_c_api.h>
 #include <permission_handler_windows/permission_handler_windows_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   FlutterWebRTCPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterWebRTCPlugin"));
-  FlutterWindowClosePluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FlutterWindowClosePlugin"));
   OpenviduClientPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("OpenviduClientPluginCApi"));
   PermissionHandlerWindowsPluginRegisterWithRegistrar(
